83a566c06b1ea19a7a8103648f91a33f10a9a968
lyynnee <ndanlinet@gmail.com>
2026-02-16 11:23:15 +0300

    chore: remove tokens from code

--- backend/src/middlewares/auth.js
+++ backend/src/middlewares/auth.js
@@ -14,6 +14,3 @@ module.exports = function auth(req, res, next) {
     return res.status(401).json({ message: "Invalid token" });
   }
 };
-// citizen token eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwicm9sZSI6IkNJVElaRU4iLCJpYXQiOjE3NzA5MDEyMDgsImV4cCI6MTc3MTUwNjAwOH0.11c_mszfxL-N5sVGJyhe_Prqbf1kFDIYyAsaDOzkkJo
-//admin token: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwicm9sZSI6IkFETUlOIiwiaWF0IjoxNzcwOTcwODQxLCJleHAiOjE3NzE1NzU2NDF9.bfNcIXmRDgbB7gvhRWnoLo9JEMJlAj5HbrZDGBfC1ao
-//collector token: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Miwicm9sZSI6IkNPTExFQ1RPUiIsImlhdCI6MTc3MDk3NTE2MywiZXhwIjoxNzcxNTc5OTYzfQ.Jg9cxxA9lMcW6Zzd3m-C_6SVQZAKkj5Bnj2pAbDkdbU 
